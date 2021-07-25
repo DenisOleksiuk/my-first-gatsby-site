aef3d6976826d192af1fa36fac7314dca27d3d30
Denis <denisolexyuk@gmail.com>
2021-07-25 17:25:00 +0300

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     siteUrl: 'https://www.yourdomain.tld',
     title: 'My Gatsby Site',
   },
-  plugins: ['gatsby-plugin-gatsby-cloud', 'gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'blog',
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
